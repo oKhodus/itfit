149f81565e153ca6d60d3dfd7f84708994a23e8f
Oleksii Khodus <exalchel@gmail.com>
2025-10-01 15:23:30 +0300

    Do weight category implement

--- main.py
+++ main.py
@@ -13,15 +13,33 @@ class BMI(User):
     BMI = weight / (height * height) ≈ res kg/m^2
     """
 
-    def __init__(self, name, age, weight, height, gender):
+    def __init__(self, name: str, age: int, weight: float, height: float, gender: str):
         super().__init__(name, age, weight, height, gender)
 
-    def calc(self):
+    def calc_BMI(self):
         recalc_height = self.height / 100
         h2 = recalc_height * recalc_height
-        result = round(self.weight / h2, 2)
-        return print(result)
+        BMIRES = round(self.weight / h2, 2)
+        return BMIRES
 
+    def define_Wcategory(self):
+        BMIRES = self.calc_BMI()
+        
+        weight_category = {
+            (18.5, 25): "Normal",
+            (25, 30): "Overweight",
+            (30, 35): "Obese: Class I",
+            (35, 40): "Obese: Class II",
+            (40, 100): "Obese: Class III"
+        }
+        for key in weight_category:
+            if BMIRES >= key[0] and BMIRES < key[1]:
+                WC = weight_category[key]
+                template_text = f"Your BMI is: {BMIRES}\nYour weight category is: {WC}"
+                return print(template_text)
+            else:
+                template_text = f"Your BMI is: {BMIRES}\nYour weight category is: Underweight"
+                return print(template_text)
 
-me = BMI("Oleksii", 20, 62, 173, "Male")
-me.calc()
+me = BMI("Oleksii", 20, 63, 173, "Male")
+me.define_Wcategory()
