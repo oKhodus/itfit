63c6f44c488f57976e55ae23037c5a3f5f3f42fa
Oleksii Khodus <exalchel@gmail.com>
2025-10-06 02:24:20 +0300

    Do refactor {define_weight_category}, add representing as other func

--- main.py
+++ main.py
@@ -17,29 +17,30 @@ class BMI(User):
         super().__init__(name, age, weight, height, gender)
 
     def calc_BMI(self):
-        recalc_height = self.height / 100
-        h2 = recalc_height * recalc_height
-        BMIRES = round(self.weight / h2, 2)
-        return BMIRES
+        squared_height = pow((self.height / 100), 2)
+        return round(self.weight / squared_height, 2)
 
-    def define_Wcategory(self):
+    def define_weight_category(self):
         BMIRES = self.calc_BMI()
-        
+
         weight_category = {
             (18.5, 25): "Normal",
             (25, 30): "Overweight",
             (30, 35): "Obese: Class I",
             (35, 40): "Obese: Class II",
-            (40, 100): "Obese: Class III"
+            (40, 100): "Obese: Class III",
         }
         for key in weight_category:
-            if BMIRES >= key[0] and BMIRES < key[1]:
-                WC = weight_category[key]
-                template_text = f"Your BMI is: {BMIRES}\nYour weight category is: {WC}"
-                return print(template_text)
-            else:
-                template_text = f"Your BMI is: {BMIRES}\nYour weight category is: Underweight"
-                return print(template_text)
+            return (
+                weight_category[key]
+                if BMIRES >= key[0] and BMIRES < key[1]
+                else "Underweight"
+            )
+
+    def represent_weight_category(self):
+        BMIRES = self.calc_BMI()
+        WC = self.define_weight_category()
+        print(f"Your BMI is: {BMIRES}\nYour weight category is: {WC}")
 
 me = BMI("Oleksii", 20, 63, 173, "Male")
-me.define_Wcategory()
+me.represent_weight_category()
