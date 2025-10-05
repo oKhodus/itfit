c80fc63af73f3489090be683aec2fc7e458f5e7d
Oleksii Khodus <exalchel@gmail.com>
2025-10-06 02:45:47 +0300

    Do restructure of project

--- main.py
+++ main.py
@@ -1,46 +1,6 @@
-# import typing
-class User:
-    def __init__(self, name: str, age: int, weight: float, height: float, gender: str):
-        self.name = name
-        self.age = age
-        self.weight = weight
-        self.height = height
-        self.gender = gender
+from stats import BMI
 
-
-class BMI(User):
-    """
-    BMI = weight / (height * height) ≈ res kg/m^2
-    """
-
-    def __init__(self, name: str, age: int, weight: float, height: float, gender: str):
-        super().__init__(name, age, weight, height, gender)
-
-    def calc_BMI(self):
-        squared_height = pow((self.height / 100), 2)
-        return round(self.weight / squared_height, 2)
-
-    def define_weight_category(self):
-        BMIRES = self.calc_BMI()
-
-        weight_category = {
-            (18.5, 25): "Normal",
-            (25, 30): "Overweight",
-            (30, 35): "Obese: Class I",
-            (35, 40): "Obese: Class II",
-            (40, 100): "Obese: Class III",
-        }
-        for key in weight_category:
-            return (
-                weight_category[key]
-                if BMIRES >= key[0] and BMIRES < key[1]
-                else "Underweight"
-            )
-
-    def represent_weight_category(self):
-        BMIRES = self.calc_BMI()
-        WC = self.define_weight_category()
-        print(f"Your BMI is: {BMIRES}\nYour weight category is: {WC}")
+print("Hello, this is welcome message from itfit!")
 
 me = BMI("Oleksii", 20, 63, 173, "Male")
 me.represent_weight_category()
---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, name: str, age: int, weight: float, height: float, gender: str):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.gender = gender
